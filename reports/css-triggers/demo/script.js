45d2b9ead99f6d52cda2cfeda3b66c93317e67bb
rastorguevila <rastorguew94@gmail.com>
2024-04-21 16:22:54 +0300

    fix title, add hidden block for demo

--- reports/css-triggers/demo/script.js
+++ reports/css-triggers/demo/script.js
@@ -1,6 +1,6 @@
 (function() {
     const SELECTORS = ['layout', 'paint', 'composite', 'moved']
-    
+
     const addListener = (button, box) => {
         button.addEventListener('click', () => {
             if (box.classList.contains('active')) {
@@ -10,6 +10,14 @@
             }
         })
     }
+
+    const sizes = document.getElementById('size');
+
+    sizes.addEventListener('click', (e) => {
+      setTimeout(() => {
+        document.querySelector('composite').getBoundingClientRect();
+      }, 1000)
+    })
 
     SELECTORS.forEach(selector => {
         const button = document.getElementById(selector);
